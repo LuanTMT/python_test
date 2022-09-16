9382ebf3f97763fcf1a57d364b56b80e541904f7
LuanTMT <103752171+LuanTMT@users.noreply.github.com>
2022-09-16 10:04:46 +0700

    ab

--- app/main.py
+++ app/main.py
@@ -1,66 +1,12 @@
-from fastapi import FastAPI, Body, Depends
-from app.database.schemas import userSchemas
-from app.database.models import models
+from fastapi import FastAPI
 from app.router import rout
-from .config import Base, engine, SessionLocal
-from sqlalchemy.orm import Session
+from .config import Base, engine
+
 
 Base.metadata.create_all(engine)
-
-
-def get_session():
-    session = SessionLocal()
-    try:
-        yield session
-    finally:
-        session.close()
-
 
 app = FastAPI()
 
 # get All
 
 app.include_router(rout)
-
-
-@app.get("/")
-def getItems(session: Session = Depends(get_session)):
-    items = session.query(models.Users).all()
-    return items
-
-
-# get 1 item
-
-
-@app.get("/{id}")
-def getOneItem(id: int, session: Session = Depends(get_session)):
-    ittem = session.query(models.Users).get(id)
-    return ittem
-
-
-@app.post("/")
-def postItem(item: userSchemas.Item, session: Session = Depends(get_session)):
-    items = models.Users(username=item.username)
-    session.add(items)
-    session.commit()
-    session.refresh(items)
-    return items
-
-
-@app.put("/{id}")
-def updateItem(
-    id: int, item: userSchemas.Item, session: Session = Depends(get_session)
-):
-    ittem = session.query(models.Item).get(id)
-    ittem.task = item.task
-    item = models.Item(task=item.task)
-    session.commit()
-    return ittem
-
-
-@app.delete("/{id}")
-def deleteItem(id: int, session: Session = Depends(get_session)):
-    session.delete(session.query(models.Item).get(id))
-    session.commit()
-    session.close()
-    return "Delete item succesfully!"
